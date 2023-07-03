9a18a19301f7004014bab0ac0de8c82b1f98dac0
sheikh-zafar <audios.shkzafar@gmail.com>
2023-07-03 19:53:16 +0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+};
