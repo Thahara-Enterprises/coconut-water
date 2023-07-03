335b5e20916146388d79610a737e6178822ae420
sheikh-zafar <audios.shkzafar@gmail.com>
2023-07-03 20:46:12 +0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { data } from './data';
+import { data } from './data/index';
 import Layout from './component/Layout';
 import ProductItem from './component/ProductItem';
 import { Store } from './utils/Store';
